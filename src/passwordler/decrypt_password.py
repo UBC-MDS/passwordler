81097f3b162b281c386724e56ea17f3e5d54388f
Michelle Hunn <mihunn@student.ubc.ca>
2024-01-12 13:28:26 -0800

    docstring format adjustment decfun

--- src/passwordler/decrypt_password.py
+++ src/passwordler/decrypt_password.py
@@ -2,7 +2,10 @@ def decrypt_password(password, key):
     """
     Decrypt an encrypted password.
 
-    This function decrypts an encrypted string. It supports the Advanced Encryption Standard (AES) encryption, which is widely used to secure sensitive data. Employing the provided key, the function decrypts the password, ensuring it matches the encryption algorithm applied during the encryption process. The function returns the original, human-readable password.
+    This function decrypts an encrypted string. It supports the Advanced Encryption Standard (AES) 
+    encryption, which is widely used to secure sensitive data. Employing the provided key, the 
+    function decrypts the password, ensuring it matches the encryption algorithm applied during the 
+    encryption process. The function returns the original, human-readable password.
 
     Parameters:
     password (bytes): The encrypted password to be decrypted.
