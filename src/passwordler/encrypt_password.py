84dc0a4c5121b5290a9a3911364de98afcc055b3
Yiwei Zhang <zywkloo@student.ubc.ca>
2024-01-12 12:56:54 -0800

    Add key as encrypt_password input

--- src/passwordler/encrypt_password.py
+++ src/passwordler/encrypt_password.py
@@ -1,14 +1,12 @@
-def encrypt_password(raw_password: str, encrypt_algo: list = ['AES']) -> str:
+def encrypt_password(raw_password: str, key: bytes, encrypt_algo: list = ['AES']) -> str:
     """
     Encrypt a given password.
-
-    This function encrypts a password using a specified encryption algorithm. Currently, it supports AES encryption.
+    This function encrypts a password using a specified encryption algorithm and key. Currently, it supports AES encryption.
     The function applies padding to the password to match the block size requirement of the encryption algorithm.
-
     Parameters:
     raw_password (str): The password to be encrypted.
+    key (bytes): The encryption key to use.
     encrypt_algo (list): List of encryption algorithms to use. Default is ['AES'].
-
     Returns:
     str: The encrypted password.
     """
