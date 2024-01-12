e88482baaf3f1a10908147ae73cbf5d94c7936c2
Yiwei Zhang <zywkloo@student.ubc.ca>
2024-01-11 20:51:23 -0800

    remove code

--- src/passwordler/password_encryption.py
+++ src/passwordler/password_encryption.py
@@ -1,6 +1,3 @@
-from Crypto.Cipher import AES
-import os
-
 def password_encryption(raw_password: str, encrypt_algo: list = ['AES']) -> str:
     """
     Encrypt a given password.
@@ -15,21 +12,3 @@ def password_encryption(raw_password: str, encrypt_algo: list = ['AES']) -> str:
     Returns:
     str: The encrypted password.
     """
-
-    # Define block size and padding function for AES
-    BS = AES.block_size
-    pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
-
-    # Only AES is implemented, for now, raise error for other algorithms
-    if 'AES' not in encrypt_algo or len(encrypt_algo) > 1:
-        raise NotImplementedError("Currently, only AES encryption is implemented.")
-
-    # Generate a random key for AES encryption
-    key = os.urandom(32)  # AES requires a key of 16, 24, or 32 bytes
-
-    # Encrypt the password
-    cipher = AES.new(key, AES.MODE_ECB)
-    encrypted_password = cipher.encrypt(pad(raw_password))
-
-    # Returning the encrypted password as a hexadecimal string for readability
-    return encrypted_password.hex()
