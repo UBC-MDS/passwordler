c6dd23a6b9fe6a57b28096d3cab497fb16663843
Michelle Hunn <mihunn@student.ubc.ca>
2024-01-16 17:27:54 -0800

    decryption function

--- src/passwordler/decrypt_password.py
+++ src/passwordler/decrypt_password.py
@@ -1,4 +1,6 @@
-def decrypt_password(password, key):
+import random
+
+def decrypt_password(encrypted_message, random_seed = 123):
     """
     Decrypt an encrypted password.
 
@@ -15,3 +17,28 @@ def decrypt_password(password, key):
     str: The decrypted password.
 
     """
+    random.seed(random_seed)
+    
+    original = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q',
+                'r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H',
+                'I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y',
+                'Z','é','è','à','ü','ö','ä','0','1','2','3','4','5','6','7','8','9',
+                '.',',',';',':','!','?','+','@','#','%','&','$','£','=','-','_',' ',]
+
+    encryption = original.copy()
+    random.shuffle(encryption)
+    
+    decrypted_msg = []
+
+    for character in encrypted_message:
+        if character in original:
+            index = encryption.index(character)
+            new_char = original[index]
+            decrypted_msg.append(new_char)
+        else:
+            decrypted_msg.append(character)
+
+    decrypted_msg = ''.join(decrypted_msg)
+
+    print(decrypted_msg)
+    return decrypted_msg
