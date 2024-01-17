7b463e93ad956c4f1998118befc037f274acba53
Yiwei Zhang <zywkloo@student.ubc.ca>
2024-01-16 19:56:41 -0800

    Add poetry.lock

--- src/passwordler/encrypt_password.py
+++ src/passwordler/encrypt_password.py
@@ -7,7 +7,7 @@ original = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
             '.', ',', ';', ':', '!', '?', '+', '@', '#', '%', '&', '$', '£', '=', '-', '_', ' ',]
 
 
-def getKeyMap(shuffled, isReverse=False):
+def getKeyMap(shuffled, isDecryption=False):
     """
     Generate a key map for encryption or decryption.
 
@@ -23,7 +23,7 @@ def getKeyMap(shuffled, isReverse=False):
     """
     keyMap = {}
     for i in range(len(shuffled)):
-        if isReverse:  # For decryption
+        if isDecryption:  # For decryption
             keyMap[shuffled[i]] = original[i]
         else:  # For encryption
             keyMap[original[i]] = shuffled[i]
@@ -62,3 +62,37 @@ def encrypt_password(message, random_seed=123):
     encrypted_msg = ''.join(encrypted_msg)
 
     return encrypted_msg
+
+
+def decrypt_password(encrypted_message, random_seed=123):
+    """
+    Decrypt a message encrypted with the simple substitution cipher.
+
+    This function decrypts a given message by mapping each character back to its original character
+    using the same shuffled character set used for encryption. The function requires the same random
+    seed that was used for encryption to ensure consistent shuffling.
+
+    Parameters:
+    encrypted_message (str): The message to be decrypted.
+    random_seed (int): The seed used for random shuffling (should be the same as used in encryption).
+
+    Returns:
+    str: The decrypted message.
+    """
+    random.seed(random_seed)
+
+    decryption = original.copy()
+    random.shuffle(decryption)
+
+    keyMap = getKeyMap(decryption, isDecryption=True)
+    decrypted_msg = []
+
+    for character in encrypted_message:
+        if character in keyMap:
+            decrypted_msg.append(keyMap[character])
+        else:
+            decrypted_msg.append(character)
+
+    decrypted_msg = ''.join(decrypted_msg)
+
+    return decrypted_msg
